6cfe9061e2d8735d4d4f0f3f9467ce5668e6b2a0
Vương Đắc Tài <taivuongdac@gmail.com>
2026-09-16 21:25:16 +0700

    "btvn 04"

--- src/main/java/spring/btvn/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/btvn/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package spring.btvn.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,16 +10,34 @@ import spring.btvn.common.reponse.ErrorResponse;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        log.warn("Resource not found at path [{}]: {}", request.getRequestURI(), ex.getMessage());
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.NOT_FOUND.value())
                         .error(HttpStatus.NOT_FOUND.getReasonPhrase())
                         .message(ex.getMessage())
+                        .path(request.getRequestURI())
+                        .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception e, HttpServletRequest request) {
+        log.error("Lỗi hệ thống xảy ra: ", e);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                        .message("Đã có lỗi xảy ra từ hệ thống, vui lòng thử lại sau!")
                         .path(request.getRequestURI())
                         .build());
     }
